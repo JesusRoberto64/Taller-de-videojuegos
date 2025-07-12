88564e0e4f77bb33963921c9f97440bd04915410
Jesús Roberto Gzz Fonsca <gamerslive20@gmail.com>
2025-07-12 02:23:53 -0500

    scroll infinito

--- Escenas/Movimiento/Simple/player.gd
+++ Escenas/Movimiento/Simple/player.gd
@@ -4,15 +4,19 @@ extends AnimatedSprite2D
 
 func _process(delta: float) -> void:
 	var mov = Vector2.ZERO
+	
 	# Botones para izquerda y derecha
 	if Input.is_action_pressed("ui_right"):
 		mov.x = mov.x + velocity * delta
 		flip_h = false # cambia la dirección del sprite
+		$Shadow.offset.x = -8.0
 		pass
 	elif Input.is_action_pressed("ui_left"):
 		mov.x = mov.x - velocity * delta
 		flip_h = true # cambia la dirección del sprite
+		$Shadow.offset.x = -4.0
 		pass
+	
 	# Botones para arriba y abajo
 	if Input.is_action_pressed("ui_down"):
 		mov.y = mov.y + velocity * delta
@@ -22,7 +26,11 @@ func _process(delta: float) -> void:
 		pass
 	
 	position += mov
-	screen_limit()
+	# Animación 
+	if mov != Vector2.ZERO:
+		play("walk")
+	else:
+		play("idle")
 	pass
 
 func screen_limit() -> void:
