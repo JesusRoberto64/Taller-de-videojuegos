0dee56987c194128747b3137359cad535bac6381
Jesús Roberto Gzz Fonsca <gamerslive20@gmail.com>
2025-10-18 03:46:51 -0500

    fantasma julio

---
+++ Escenas/Coop/Fantasmas/fantasma_05.gd
@@ -0,0 +1,26 @@
+extends Fantasma
+
+var vec_direction : Vector2 = Vector2(1.0,1.0)
+var target = null
+
+func _ready() -> void:
+	anim_player.connect('animation_finished',_on_animation_finished)
+	connect("area_entered", _on_area_entered)
+	connect("area_exited", _on_area_exited)
+	invoke()
+	target = get_tree().get_nodes_in_group("Player").pick_random()
+
+func _physics_process(delta: float) -> void:
+	fading(delta)
+	movement(delta)
+
+func movement(delta) -> void:
+	if !can_move: return
+	position = lerp(position, target.position, delta)
+
+func revive() -> void:
+	position = Vector2(randf_range(36.0, 280.0), randf_range(36.0, 150.0))
+	pos = position
+	hp = maxHp
+	anim_player.play("apearing")
+	target = get_tree().get_nodes_in_group("Player").pick_random()
---
+++ Escenas/Coop/Fantasmas/fantasma_07.gd
@@ -0,0 +1,38 @@
+extends Fantasma
+
+# Fantasma movimento arriva abajo
+
+var is_close = true
+var targets : Array = []
+var target = null
+
+func _ready() -> void:
+	targets = get_tree().get_nodes_in_group("Player")
+	invoke()
+
+func _physics_process(delta: float) -> void:
+	fading(delta)
+	movement(delta)
+	
+	if !is_close : return
+	for t in targets:
+		if t.position.distance_to(position) < 80.0:
+			is_close = false
+			target = t
+			spr.play("attack")
+			break
+	
+
+func movement(delta) -> void:
+	if !can_move : return
+	if is_close: return
+	position = lerp(position, target.position, delta)
+
+func revive() -> void:
+	position = Vector2(randf_range(36.0, 280.0), randf_range(36.0, 150.0))
+	pos = position
+	hp = maxHp
+	anim_player.play("apearing")
+	
+	spr.play("default")
+	is_close = true
