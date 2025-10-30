81cdf6949b0352fe5c6bc9fe0c5b451812684f7d
JesusRoberto64 <40547350+JesusRoberto64@users.noreply.github.com>
2025-10-30 16:40:24 -0600

    shaoder y particle warmer

--- Escenas/Coop/Logica/shader_warmup.gd
+++ Escenas/Coop/Logica/shader_warmup.gd
@@ -10,20 +10,22 @@ var particles = [
 	preload("res://Escenas/Coop/Escenas/particles_apearing.tscn")
 ]
 
-func _process(_delta: float) -> void:
+var run = true
+
+func _process(delta: float) -> void:
+	if not run: return
 	for mat in shaders:
 		var s = Sprite2D.new()
 		s.material = mat
 		add_child(s)
-		s.hide()
 	
-	#for par in particles:
-		#var p = par.instantiate()
-		#add_child(p)
-		#p.visible = false
-		#p.emitting = true
-		#await get_tree().create_timer(0.25).timeout
-		#p.emitting = false
-	
-	queue_free()
-	pass
+	for par in particles:
+		var p = par.instantiate()
+		add_child(p)
+		p.emitting = true
+	run = false
+
+func _on_timer_timeout() -> void:
+	for p in get_children():
+		if p is GPUParticles2D:
+			p.emitting = false
