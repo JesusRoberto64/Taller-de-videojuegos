78f39520dc20b7cfe8eb1e56ed5fd8a335009ca2
JesusRoberto64 <40547350+JesusRoberto64@users.noreply.github.com>
2025-11-28 22:17:08 -0600

    pollito salto TEP

--- Escenas/Pollito/pollito.gd
+++ Escenas/Pollito/pollito.gd
@@ -1,12 +1,19 @@
 extends CharacterBody2D
 
-enum ANIM_STATE {JUMPING, GROUNDING}
-var cur_anim_state = ANIM_STATE.GROUNDING
+enum ANIM_STATE {JUMPING, GROUNDING, FLYING}
+var cur_anim_state = ANIM_STATE.JUMPING
 
-var jump_force = 70.0 #100.0
-var jump_counter : int = 2
-var jump_mult = 3.0
-var gravity = 10.0
+enum STATE { JUMPING, GROUND, FLY }
+var cur_state = STATE.JUMPING
+
+var jump_force = 225.0
+var jump_impulse = 5.0
+var can_impulse_jump = false
+
+var fly_force = 100.0
+var gravity_fall = 15.0
+var gravity_jump = 12.0
+var gravity_fly = 10.0
 
 var speed = 50.0
 var direction = 1.0
@@ -18,18 +25,37 @@ func _physics_process(_delta: float) -> void:
 	if move != 0.0: direction = move
 	
 	if is_on_floor():
-		cur_anim_state = ANIM_STATE.GROUNDING
-		jump_counter = 2
-		jump_mult = jump_counter
-		velocity.y = 0.0
-	else:
-		cur_anim_state = ANIM_STATE.JUMPING
-		velocity.y += gravity
+		cur_state = STATE.GROUND
+	elif cur_state == STATE.GROUND:
+		cur_state = STATE.JUMPING
 	
-	if Input.is_action_just_pressed('ui_accept') and jump_counter > 0:
-		velocity.y = -jump_force * jump_mult
-		jump_counter -= 1
-		jump_mult = jump_counter + 1
+	match cur_state:
+		STATE.GROUND:
+			if Input.is_action_just_pressed('ui_accept'):
+				velocity.y = -jump_force
+				cur_state = STATE.JUMPING
+				can_impulse_jump = true
+			
+			cur_anim_state = ANIM_STATE.GROUNDING
+		STATE.JUMPING:
+			velocity.y += gravity_fall if velocity.y > 0.0 else gravity_jump
+			
+			if can_impulse_jump and Input.is_action_pressed('ui_accept') and velocity.y < 0.0:
+				velocity.y -= jump_impulse
+			elif Input.is_action_just_released('ui_accept'):
+				can_impulse_jump = false
+			
+			if Input.is_action_just_pressed('ui_accept'):
+				velocity.y = -fly_force
+				cur_state = STATE.FLY
+			
+			cur_anim_state = ANIM_STATE.JUMPING
+		STATE.FLY:
+			velocity.y += gravity_fly
+			if Input.is_action_just_pressed('ui_accept'):
+				velocity.y = -fly_force
+			
+			cur_anim_state = ANIM_STATE.JUMPING
 	
 	velocity.x = move * speed
 	move_and_slide()
@@ -46,6 +72,8 @@ func _process(_delta: float) -> void:
 				$AnimatedSprite2D.play("fall")
 			else:
 				$AnimatedSprite2D.play("jump")
+		ANIM_STATE.FLYING:
+			pass
 		ANIM_STATE.GROUNDING:
 			if move_anim == 0.0:
 				$AnimatedSprite2D.play("idle")
